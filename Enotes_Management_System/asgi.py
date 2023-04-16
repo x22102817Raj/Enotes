3c19e05f5c816688f19bb32b32f324817098251b
saran KS <sks@presidio.com>
2023-04-17 00:54:54 +0530

    enotes commit

---
+++ Enotes_Management_System/asgi.py
@@ -0,0 +1,15 @@
+"""
+ASGI config for Enotes_Management_Systemproject.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Enotes_Management_System.settings')
+
+application = get_asgi_application()
---
+++ enotes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Signup,Notes
+# Register your models here.
+
+admin.site.register(Signup)
+admin.site.register(Notes)
